ed690aa72f513af07d19bffb0a2c673ebc91748b
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-07-12 12:45:09 -0700

    Update ping.js

--- src/commands/util/ping.js
+++ src/commands/util/ping.js
@@ -1,28 +1,41 @@
-const { oneLine } = require('common-tags');
 const Command = require('../base');
-
+const Discord = require('discord.js');
+const moment = require('moment');
 module.exports = class PingCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'ping',
-			group: 'util',
-			memberName: 'ping',
-			description: 'Checks the bot\'s ping to the Discord server.',
-			throttling: {
-				usages: 5,
-				duration: 10
-			}
-		});
-	}
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            group: 'bot',
+            memberName: 'ping',
+            description: 'Shows the ping for the bot',
+            examples: ['ping'],
+            aliases: ["pong", "pung"],
+            guildOnly: false,
+            throttling: {
+                usages: 1,
+                duration: 2
+            },
+        });
+    }
 
-	async run(msg) {
-		const pingMsg = await msg.reply('Pinging...');
-		return pingMsg.edit(oneLine`
-			${msg.channel.type !== 'dm' ? `${msg.author},` : ''}
-			Pong! The message round-trip took ${
-				(pingMsg.editedTimestamp || pingMsg.createdTimestamp) - (msg.editedTimestamp || msg.createdTimestamp)
-			}ms.
-			${this.client.ws.ping ? `The heartbeat ping is ${Math.round(this.client.ws.ping)}ms.` : ''}
-		`);
-	}
+    async run(msg) {
+        try{
+        let loadingembed = new Discord.RichEmbed()
+            .setColor(this.client.util.colors.cyan)
+            .setDescription(`${this.client.util.emojis.eload} Loading.`)
+            .setTimestamp()
+        const message = await msg.channel.send(loadingembed);
+        let embed = new Discord.RichEmbed()
+            .setColor(this.client.util.colors.cyan)
+            .setTitle(`${this.client.util.emojis.robot} Status ${this.client.util.emojis.robot}`)
+            .setFooter(msg.author.tag, msg.author.displayAvatarURL())
+            .addField(`Message Latency`, `${message.createdTimestamp - msg.createdTimestamp}ms`, true)
+            .addField(`API Latency`, `${Math.round(this.client.ws.ping)}ms`, true)
+            .addField(`Uptime`, `${moment.duration(this.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]")}`, true)
+            .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL())
+        message.edit(embed);
+        }catch(e){
+		msg.channel.send(`Error while running command:\n${e}`)
+        }
+    }
 };

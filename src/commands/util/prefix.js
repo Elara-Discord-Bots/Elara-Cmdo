9e464a54d3df0b7197041fbfa9af3ef875461880
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-07-22 16:10:02 -0700

    Update prefix.js

--- src/commands/util/prefix.js
+++ src/commands/util/prefix.js
@@ -1,67 +1,73 @@
-const { stripIndents, oneLine } = require('common-tags');
-const Command = require('../base');
+const {Command} = require('elaracmdo'), {MessageEmbed} = require('discord.js');
+module.exports = class NCommand extends Command {
+         constructor(client) {
+           super(client, {
+             name: 'prefix',
+             memberName: 'prefix',
+             aliases: [`setprefix`],
+             examples: [`${client.commandPrefix}prefix`],
+             description: 'Checks the prefix',
+             group: 'bot',
+             throttling: {
+              usages: 2,
+              duration: 5
+            },
+            args: [
+              {
+                key: "prefix",
+                prompt: "What do you want the new prefix to be?",
+                type: "string",
+                default: "",
+                min: 1,
+                max: 150
+              }
+            ]
+})
+}
+        async run(message, {prefix}) {
+        if(await this.client.b(this.client, message) === true) return;
+        if(await this.client.m(this.client) === true && !this.client.isOwner(message.author.id)) return this.client.f.msg(message)
+        if(await this.client.f.channel(this.client, message) === true) return this.client.f.cmdschannel(message);
+        
+        try{
+        let e = new MessageEmbed().setColor(message.guild.color);
+        if(message.guild){
+          this.client.db.findOne({guildID: message.guild.id}, async (err, db) => {
+            if(db){
+              if(prefix !== ""){
+              if(!message.member.hasPermission("MANAGE_GUILD")) return this.client.error(this.client, message, `You need the \`Manage Server\` permission to change the prefix!`);
+              if(prefix.toLowerCase() === "reset" || prefix.toLowerCase() === "default" || db.prefix.toLowerCase() === prefix.toLowerCase()){
+              db.prefix = "";
+              db.save().catch(err => console.log(err));
+              message.guild._commandPrefix = this.client.commandPrefix;
+              e.setDescription(`Use \`${this.client.commandPrefix}\` for commands!`).setTitle(`Prefix reset`);
+              return message.channel.send(e)
+              }else{
+                db.prefix = prefix.toLowerCase();
+                db.save().catch(err => console.log(err));
+                message.guild._commandPrefix = prefix;
+                e.setDescription(`Use \`${prefix}\` for commands!`).setTitle(`New Prefix`);
+                return message.channel.send(e)
+              }
+              }else{
+              e.setTitle(`Prefix`).setAuthor(message.guild.name, message.guild.iconURL()).setDescription(db.prefix || this.client.commandPrefix)
+              if(message.member.hasPermission("MANAGE_GUILD")) e.setFooter(`Note: To change the prefix do ${db.prefix || this.client.commandPrefix}setprefix [newprefix]`);
+              return message.channel.send(e)
+              }
+            }else{
+              e.setTitle(`Prefix`).setAuthor(message.guild.name, message.guild.iconURL()).setDescription(this.client.commandPrefix)
+              if(message.member.hasPermission("MANAGE_GUILD")) e.setFooter(`Note: To change the prefix do ${this.client.commandPrefix}setprefix [newprefix]`);
+              return message.channel.send(e)
+            }
+          });
+        }else{
+          e.setTitle(`Prefix`).setDescription(this.client.commandPrefix)
+          return message.channel.send(e)
+        }
 
-module.exports = class PrefixCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'prefix',
-			group: 'util',
-			memberName: 'prefix',
-			description: 'Shows or sets the command prefix.',
-			format: '[prefix/"default"/"none"]',
-			details: oneLine`
-				If no prefix is provided, the current prefix will be shown.
-				If the prefix is "default", the prefix will be reset to the bot's default prefix.
-				If the prefix is "none", the prefix will be removed entirely, only allowing mentions to run commands.
-				Only administrators may change the prefix.
-			`,
-			examples: ['prefix', 'prefix -', 'prefix omg!', 'prefix default', 'prefix none'],
-
-			args: [
-				{
-					key: 'prefix',
-					prompt: 'What would you like to set the bot\'s prefix to?',
-					type: 'string',
-					max: 15,
-					default: ''
-				}
-			]
-		});
-	}
-
-	async run(msg, args) {
-		// Just output the prefix
-		if(!args.prefix) {
-			const prefix = msg.guild ? msg.guild.commandPrefix : this.client.commandPrefix;
-			return msg.reply(stripIndents`
-				${prefix ? `The command prefix is \`\`${prefix}\`\`.` : 'There is no command prefix.'}
-				To run commands, use ${msg.anyUsage('command')}.
-			`);
-		}
-
-		// Check the user's permission before changing anything
-		if(msg.guild) {
-			if(!msg.member.hasPermission('ADMINISTRATOR') && !this.client.isOwner(msg.author)) {
-				return msg.reply('Only administrators may change the command prefix.');
-			}
-		} else if(!this.client.isOwner(msg.author)) {
-			return msg.reply('Only the bot owner(s) may change the global command prefix.');
-		}
-
-		// Save the prefix
-		const lowercase = args.prefix.toLowerCase();
-		const prefix = lowercase === 'none' ? '' : args.prefix;
-		let response;
-		if(lowercase === 'default') {
-			if(msg.guild) msg.guild.commandPrefix = null; else this.client.commandPrefix = null;
-			const current = this.client.commandPrefix ? `\`\`${this.client.commandPrefix}\`\`` : 'no prefix';
-			response = `Reset the command prefix to the default (currently ${current}).`;
-		} else {
-			if(msg.guild) msg.guild.commandPrefix = prefix; else this.client.commandPrefix = prefix;
-			response = prefix ? `Set the command prefix to \`\`${args.prefix}\`\`.` : 'Removed the command prefix entirely.';
-		}
-
-		await msg.reply(`${response} To run commands, use ${msg.anyUsage('command')}.`);
-		return null;
-	}
-};
+          } catch (e) {
+            this.client.error(this.client, message, e);
+            this.client.f.logger(this.client, message, e.stack)
+          }
+}
+}

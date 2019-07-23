60af8e0d9a3d0c985f118a19b49e3b71baf820ef
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-07-22 17:09:10 -0700

    Update support.js

--- src/commands/util/support.js
+++ src/commands/util/support.js
@@ -21,7 +21,7 @@ module.exports = class SCommand extends Command {
         if(await this.client.f.channel(this.client, message) === true) return this.client.f.cmdschannel(message);
         let embed = new Discord.MessageEmbed()
             .setAuthor(`Elara Support`, "https://cdn.discordapp.com/icons/499409162661396481/28c6fa39e722e2c0aea60f15ca105c1d.png?size=2048")
-            .setColor(this.client.util.colors.cyan)
+            .setColor(message.guild ? message.guild.color : this.client.util.colors.default)
             .setDescription(this.client.options.invite)
         message.say(embed)
     }

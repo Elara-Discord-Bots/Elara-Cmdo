9e4e10995ba257b9835321eb7d239933642e4cac
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-07-22 17:08:33 -0700

    Update info.js

--- src/commands/util/info.js
+++ src/commands/util/info.js
@@ -23,24 +23,20 @@ module.exports = class BotinfoCommand extends Command {
         if(await this.client.m(this.client) === true && !this.client.isOwner(message.author.id)) return this.client.f.msg(message)
         if(await this.client.f.channel(this.client, message) === true) return this.client.f.cmdschannel(message);
         try{
-        let prefix = this.client.commandPrefix;
+        let prefix = message.guild ? message.guild._commandPrefix : this.client.commandPrefix;
         let bot = this.client.user;
         let botembed = new Discord.MessageEmbed()
             .setTitle(`Bot Information`)
             .setAuthor(bot.tag, bot.displayAvatarURL())
-            .setColor(this.client.util.colors.cyan)
+            .setColor(message.guild ? message.guild.color : this.client.util.colors.default)
             .setThumbnail(this.client.user.displayAvatarURL())
             .addField(`User Info`, `
-            **User: ** ${bot}
-            **Tag: **${bot.tag}
-            **ID: **${bot.id}
-            **Discriminator: **#${bot.discriminator}
+            **User: ** ${bot} \`@${bot.tag}\` (${bot.id})
             **Avatar: **[Click Here](${bot.displayAvatarURL()})
             **Created At: **${moment(this.client.user.createdAt).format('MMMM Do YYYY, h:mm:ssa')}`, false)
             .addField(`Info`, `
             **Prefixes:** ${prefix}, ${this.client.user}
             **Mutual Servers: **${this.client.guilds.filter(g => g.members.get(message.author.id)).size}
-            **Server List: ** Do \`${prefix}servers\`
             **Stats: ** Do \`${prefix}stats\`
             **Bot Owners: **${this.client.owners.map(c => c.tag).join(' | ')}`, true)
             .addField(`Links`, `

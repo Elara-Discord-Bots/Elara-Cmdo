9f4075b9f49783500204ea23d67c28db6543b49d
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-07-22 17:09:43 -0700

    Update uptime.js

--- src/commands/util/uptime.js
+++ src/commands/util/uptime.js
@@ -25,7 +25,7 @@ module.exports = class PingCommand extends Command {
         if(await this.client.m(this.client) === true && !this.client.isOwner(msg.author.id)) return this.client.f.msg(msg)
         if(await this.client.f.channel(this.client, msg) === true) return this.client.f.cmdschannel(msg);
         let embed = new Discord.MessageEmbed()
-            .setColor('#00ffe9')
+            .setColor(msg.guild ? msg.guild.color : this.client.util.colors.default)
             .setTitle(`${this.client.util.emojis.robot} Uptime ${this.client.util.emojis.robot}`)
             .setFooter(msg.author.tag, msg.author.displayAvatarURL())
             .setDescription(`${moment.duration(this.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]")}`)
